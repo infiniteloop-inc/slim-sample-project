620e37a18898d8acfc345395fc4af7fde4523ccd
m-sei <m-sei@infiniteloop.co.jp>
2023-01-19 10:16:17 +0900

    Fix 'Hello, world' with using Slim Framework

--- api/public/index.php
+++ api/public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "Hello, world";
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
