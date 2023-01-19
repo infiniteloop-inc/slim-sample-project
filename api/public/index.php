bcb9933c03065f8cea13a19e2dc032c476d9bb7f
m-sei <m-sei@infiniteloop.co.jp>
2023-01-19 15:42:02 +0900

    Add DI container

--- api/public/index.php
+++ api/public/index.php
@@ -2,19 +2,19 @@
 
 declare(strict_types=1);
 
+use DI\Bridge\Slim\Bridge;
 use Dotenv\Dotenv;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "//..//");
 $dotenv->load();
 
-$app = AppFactory::create();
+$app = Bridge::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
+$app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
