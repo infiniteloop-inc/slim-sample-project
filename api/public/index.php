2c7053af5150335a12e3d11a5bfda1638e91a9b5
m-sei <m-sei@infiniteloop.co.jp>
2023-01-19 14:21:53 +0900

    Add phpdotenv

--- api/public/index.php
+++ api/public/index.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . "//..//");
+$dotenv->load();
 
 $app = AppFactory::create();
 
